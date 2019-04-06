6abb60bc12e0e947c621bde04a57be71ab69f1df
Lucas Johnson <contact@lucasjohnson.co.nz>
2019-04-06 19:43:35 -0400

    css, js, image compression tasks created and tested

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,83 @@
 const gulp = require("gulp");
+const newer = require('gulp-newer');
+const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
+const deporder = require('gulp-deporder');
+const stripdebug = require('gulp-strip-debug');
+const uglify = require('gulp-uglify');
 const sass = require("gulp-sass");
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('autoprefixer');
 const postcss = require("gulp-postcss");
 const cssnano = require("cssnano");
+const assets = require('postcss-assets');
+const mqpacker = require('css-mqpacker');
 const browserSync = require("browser-sync").create();
 
-const input = './src/scss/**/*.scss';
-const output = './dist/css';
+const devBuild = (process.env.NODE_ENV !== 'production');
 
-const sassOptions = {
-  errLogToConsole: true,
-  outputStyle: 'expanded'
+// install css reset
+
+const folder = {
+  src: './src/',
+  build: './build/'
 };
 
-const autoprefixerOptions = {
-  browsers: ['last 2 versions', '> 1%']
-};
+function images() {
+  const out = folder.build + 'images/';
+  return(
+    gulp
+      .src(folder.src + 'images/**/*')
+      .pipe(newer(out))
+      .pipe(imagemin({ optimizationLevel: 5 }))
+      .pipe(gulp.dest(out))
+  );
+}
+
+function js() {
+
+  const jsBuild = gulp.src(folder.src + 'js/**/*')
+    .pipe(deporder())
+    .pipe(concat('main.js'));
+
+  if (!devBuild) {
+    jsBuild = jsBuild
+      .pipe(stripdebug)
+      .pipe(uglify);
+  }
+  return jsBuild.pipe(gulp.dest(folder.build + 'js/'));
+}
 
 function css() {
+
+  const postCssOptions = [
+    assets({ loadPaths: ['images/'] }),
+    autoprefixer({ browsers: ['last 2 versions', '> 2%'] }),
+    mqpacker
+  ];
+
+  if (!devBuild) {
+    postCssOptions.push(cssnano);
+  }
+
   return (
     gulp
-      .src(input)
+      .src(folder.src + 'scss/main.scss')
       .pipe(sourcemaps.init())
-      .pipe(sass(sassOptions))
-      .on("error", sass.logError)
-      .pipe(postcss([autoprefixer(), cssnano()]))
+      .pipe(sass({
+        outputStyle: 'expanded',
+        imagePath: 'images/',
+        precision: 3,
+        errLogToConsole: true
+      }))
+      .on('error', sass.logError)
+      .pipe(postcss(postCssOptions))
       .pipe(sourcemaps.write())
-      .pipe(gulp.dest(output))
-      .pipe(browserSync.stream())
-  );
+      .pipe(gulp.dest(folder.build + 'css/'))
+  )
 }
 
-function html() {
-  return (
-    gulp
-      .src('./src/index.html')
-      .pipe(gulp.dest('./dist'))
-  );
-}
 
-function reload() {
-  browserSync.reload();
-}
-
-function watch() {
-  browserSync.init({
-    server: {
-      baseDir: './dist'
-    }
-  });
-  gulp.watch(input, css);
-  gulp.watch('./src/**/*.html', html);
-  gulp.watch('./dist', reload);
-}
-
+exports.images = images;
+exports.js = js;
 exports.css = css;
-exports.watch = watch;
